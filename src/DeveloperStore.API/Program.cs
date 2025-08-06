7de04d2d288601e8da61cc864b251800c9906512
Renan Porcino Leme <leme.renan@gmail.com>
2025-08-06 18:17:04 -0300

    Estruturação dos packages e versões e registros de rebus, automapper e mediatr no start.

--- src/DeveloperStore.API/Program.cs
+++ src/DeveloperStore.API/Program.cs
@@ -1,3 +1,9 @@
+using AutoMapper;
+using DeveloperStore.CrossCutting.Mappers;
+using MediatR;
+using Rebus.Config;
+using Rebus.Transport.InMem;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,27 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ServiceFactory>(sp => type => sp.GetRequiredService(type));
+builder.Services.AddSingleton<IMediator, Mediator>();
+
+// Register MediatR handlers
+
+//Automaper
+builder.Services.AddSingleton<IMapper>(sp =>
+{
+    var config = new MapperConfiguration(cfg =>
+    {
+        cfg.AddProfile<MappingProfile>();
+    });
+
+    return config.CreateMapper();
+});
+
+
+//Rebus
+builder.Services.AddRebus(configure => configure
+    .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), "developerstore-queue")));
 
 var app = builder.Build();
 
---
+++ src/DeveloperStore.CrossCutting/Mappers/MappingProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace DeveloperStore.CrossCutting.Mappers
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+            // CreateMap<Sale, SaleDto>();
+        }
+    }
+}
