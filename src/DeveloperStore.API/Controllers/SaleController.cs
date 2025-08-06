475eab8916465ac6c3e7aa361e5cb87f908d951f
Renan Porcino Leme <leme.renan@gmail.com>
2025-08-06 19:05:39 -0300

    Estruturação de command, entities, controller, handler.

---
+++ src/DeveloperStore.API/Controllers/SaleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeveloperStore.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SaleController : Controller
+    {
+        [HttpGet(Name = "Sales")]
+        public IActionResult Index()
+        {
+            return Ok(new {Result = "1, 2, 3"});
+        }
+    }
+}
